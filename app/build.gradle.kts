29e1b05fd64db2a5963727281e8b0a03ad375b85
Shem Tuti <shemtuti@gmail.com>
2023-10-12 17:08:20 +0300

    detekt issues resolved -3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.anime"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
